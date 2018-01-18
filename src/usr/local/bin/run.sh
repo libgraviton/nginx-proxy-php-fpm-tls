e11ef8b1e892288761b7d606043887f5709e3cc3
narcoticfresh <dn@xbe.ch>
2018-01-18 13:34:59 +0100

    clearer structure

--- src/usr/local/bin/run.sh
+++ src/usr/local/bin/run.sh
@@ -10,16 +10,16 @@ PROXYMODE=none
 # php-fpm proxy mode
 if [ ! -z ${FPM_UPSTREAM+x} ] && [ ! -z ${FPM_PATH+x} ]; then
     # replace the configured fpm upstream by env
-    sed -i 's@FPMUPSTREAMHOSTNAME@'"$FPM_UPSTREAM"'@' /etc/nginx/templates/upstream-fpm-upstream.conf
+    sed -i 's@FPMUPSTREAMHOSTNAME@'"$FPM_UPSTREAM"'@' /etc/nginx/templates/global/upstream-fpm-upstream.conf
 
     # replace FPM path (filename of upstream)
-    sed -i 's@FPMUPSTREAMPATH@'"$FPM_PATH"'@' /etc/nginx/templates/upstream-fpm-server.conf
+    sed -i 's@FPMUPSTREAMPATH@'"$FPM_PATH"'@' /etc/nginx/templates/proxies/upstream-fpm-server.conf
 
     # replace local static webroot
-    sed -i 's@FPMSTATICWEBROOT@'"$FPM_STATIC_WEBROOT"'@' /etc/nginx/templates/upstream-fpm-server.conf
+    sed -i 's@FPMSTATICWEBROOT@'"$FPM_STATIC_WEBROOT"'@' /etc/nginx/templates/proxies/upstream-fpm-server.conf
 
     # copy the php upstream block to its final destination
-    cp /etc/nginx/templates/upstream-fpm-upstream.conf /etc/nginx/conf.d/dynamic/upstreams/upstream.conf
+    cp /etc/nginx/templates/global/upstream-fpm-upstream.conf /etc/nginx/conf.d/dynamic/global/upstream.conf
 
     # the server block relevant part to its final destination
     cp /etc/nginx/templates/proxies/upstream-fpm-server.conf /etc/nginx/conf.d/dynamic/proxy.conf
@@ -43,18 +43,18 @@ fi
 # tls client cert
 if [ ! -z ${CLIENT_TLS_CERT+x} ]; then
     # client tls cert
-    sed -i 's@CLIENTTLSCERT@'"$CLIENT_TLS_CERT"'@g' /etc/nginx/templates/tls-cert-auth.conf
+    sed -i 's@CLIENTTLSCERT@'"$CLIENT_TLS_CERT"'@g' /etc/nginx/templates/server/tls-cert-auth.conf
 
     # the server block relevant part to its final destination
-    cp /etc/nginx/templates/tls-cert-auth.conf /etc/nginx/conf.d/dynamic/variables/zz-tls-cert-auth.conf
+    cp /etc/nginx/templates/tls-cert-auth.conf /etc/nginx/conf.d/dynamic/server/zz-tls-cert-auth.conf
 fi
 
 # conditional basic auth
-sed -i 's@CONDITIONALBASICAUTH@'"$ENABLE_CONDITIONAL_BASIC_AUTH"'@' /etc/nginx/conf.d/dynamic/variables/conditional_basic_auth.conf
+sed -i 's@CONDITIONALBASICAUTH@'"$ENABLE_CONDITIONAL_BASIC_AUTH"'@' /etc/nginx/conf.d/dynamic/server/conditional_basic_auth.conf
 
 # ssl cert paths
-sed -i 's@SSLCERTPATH@'"$SSL_CERT"'@' /etc/nginx/conf.d/dynamic/variables/ssl_certs.conf
-sed -i 's@SSLCERTKEYPATH@'"$SSL_CERT_KEY"'@' /etc/nginx/conf.d/dynamic/variables/ssl_certs.conf
+sed -i 's@SSLCERTPATH@'"$SSL_CERT"'@' /etc/nginx/conf.d/dynamic/server/ssl_certs.conf
+sed -i 's@SSLCERTKEYPATH@'"$SSL_CERT_KEY"'@' /etc/nginx/conf.d/dynamic/server/ssl_certs.conf
 
 # timeout
 sed -i 's@KEEPALIVETIMEOUT@'"$KEEPALIVE_TIMEOUT"'@' /etc/nginx/nginx.conf
@@ -66,10 +66,10 @@ for var in ${!RULES_*}; do
     IFS='#' read -ra ADDR <<< "${!var}"
 
     if [ ! -z "${ADDR[0]}" ] && [ ! -z "${ADDR[0]}" ]; then
-        sed 's@THELOCATION@'"${ADDR[0]}"'@' /etc/nginx/templates/location-rule.conf > /tmp/config
+        sed 's@THELOCATION@'"${ADDR[0]}"'@' /etc/nginx/templates/server/location-rule.conf > /tmp/config
         sed -i 's@THEMETHODS@'"${ADDR[1]}"'@' /tmp/config
 
-        mv /tmp/config /etc/nginx/conf.d/dynamic/variables/rule-${COUNTER}.conf
+        mv /tmp/config /etc/nginx/conf.d/dynamic/server/rule-${COUNTER}.conf
 
         COUNTER=$[COUNTER+1]
     fi
