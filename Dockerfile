24f4fba6bc7241f420c7233e2ba493e26b49346c
narcoticfresh <dn@xbe.ch>
2018-12-04 12:56:33 +0100

    generate more features with our php generator

--- Dockerfile
+++ Dockerfile
@@ -11,26 +11,36 @@ LABEL TAG=${TAG}
 ENV TINI_VERSION v0.18.0
 ENV DEBIAN_FRONTEND noninteractive
 
+# global nginx settings
 ENV RESOLVER=none
 ENV RESOLVER_VALID=30s
-ENV SERVERNAME=localhost
-
+ENV WORKER_PROCESSES=6
+ENV WORKER_CONNECTIONS=1024
+ENV CLIENT_BODY_BUFFER_SIZE=15M
+ENV CLIENT_MAX_BODY_SIZE=15M
+ENV KEEPALIVE_TIMEOUT=200
 ENV SSL_CERT=/certs/fullchain.crt
 ENV SSL_CERT_KEY=/certs/fullchain.key
 ENV FPM_STATIC_WEBROOT=/var/www/web/
 ENV FPM_STATUS_ALLOWED_NETWORK="172.0.0.0/8"
-ENV ENABLE_CONDITIONAL_BASIC_AUTH="NO"
-ENV KEEPALIVE_TIMEOUT=200
-ENV PROXY_STANDARD_FORWARD_PROTO="\$scheme"
-ENV PROXY_STANDARD_FORWARD_PORT="\$server_port"
+ENV ENABLE_HTTP2="true"
 ENV GZIP_ENABLED="on"
 ENV GZIP_TYPES="text/css text/plain text/javascript application/javascript application/json application/x-javascript text/xml application/xml application/xml+rss application/xhtml+xml application/x-font-ttf application/x-font-opentype application/vnd.ms-fontobject image/svg+xml image/x-icon application/rss+xml application/atom_xml text/x-gettext-translation"
-# envs for conditional basic auth settings
+ENV PROXY_STANDARD_FORWARD_PROTO="\$scheme"
+ENV PROXY_STANDARD_FORWARD_PORT="\$server_port"
 ENV CONDITIONAL_BASIC_AUTH_HEADER="http_x_forwarded_for"
 ENV CONDITIONAL_BASIC_AUTH_REGEX="~172\..*"
-ENV EXPOSE_PATH "/"
-ENV ENVIRONMENT_JSON_PREFIX "ADMIN_"
-ENV DEFAULT_SERVE "none"
+
+# server (vhost) specific settings
+ENV SERVERNAME=localhost
+ENV ENABLE_CONDITIONAL_BASIC_AUTH="NO"
+ENV EXPOSE_PATH="/"
+ENV DEFAULT_SERVE="none"
+
+# environment.json related
+ENV ENVIRONMENT_JSON_PREFIX="ADMIN_"
+
+###### END CONFIGURATION
 
 ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
 
@@ -51,7 +61,7 @@ ADD src /
 COPY --from=0 /app /opt/configurator
 
 # chmod conf dir so www-data can write to
-RUN chown -R www-data:root /var/www/ /etc/nginx/ /usr/local/bin/run.sh /var/log/nginx/ /var/run/ /var/cache/nginx/ && \
+RUN chown -R www-data:root /var/www/ /etc/nginx/ /usr/local/bin/run.sh /var/log/nginx/ /var/run/ /var/cache/nginx/ /opt/configurator && \
     chmod -R go+rwx /var/www/ /etc/nginx/ /usr/local/bin/run.sh /var/log/nginx/ /var/run/ /var/cache/nginx/ && \
     chmod +x /usr/local/bin/run.sh && \
     chmod +x /tini
---
+++ src/configurator/configure.php
@@ -0,0 +1,162 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+/**
+ * for dev purposes
+ */
+/*
+$_ENV = [
+    'SERVERNAME' => 'localhost',
+    'WORKER_PROCESSES' => 6,
+    'WORKER_CONNECTIONS' => 1024,
+    'KEEPALIVE_TIMEOUT' => 200,
+    'CLIENT_BODY_BUFFER_SIZE' => '15M',
+    'CLIENT_MAX_BODY_SIZE' => '15M',
+    'GZIP_ENABLED' => 'on',
+    'GZIP_TYPES' => 'text/html',
+    'ENABLE_HTTP2' => 'true',
+    'SSL_CERT' => '/certs/fullchain.crt',
+    'SSL_CERT_KEY' => '/certs/fullchain.key',
+    'RESOLVER' => 'none',
+    'RESOLVER_VALID' => '30s',
+    'PROXY_STANDARD_FORWARD_PROTO' => 'http',
+    'PROXY_STANDARD_FORWARD_PORT' => '332',
+    'CONDITIONAL_BASIC_AUTH_HEADER' => 'http_x_forwarded_for',
+    'CONDITIONAL_BASIC_AUTH_REGEX' => '~172\..*',
+    'EXPOSE_PATH' => "/",
+    'FPM_STATIC_WEBROOT' => '/var/www',
+    'FPM_STATUS_ALLOWED_NETWORK' => '172.*',
+
+    'VHOST_GRV_SERVERNAME' => 'grv',
+     'VHOST_GRV_FPM_PATH' => '/var/www/vendor/graviton/graviton/web/app.php',
+     'VHOST_GRV_FPM_UPSTREAM' => 'graviton',
+    'VHOST_GRV_BASIC_USERNAME' => 'hans',
+    'VHOST_GRV_BASIC_PASSWORD' => 'hans',
+
+    'VHOST_GW_SERVERNAME' => 'gw',
+    'VHOST_GW_FPM_PATH' => '/var/www/app.php',
+    'VHOST_GW_FPM_UPSTREAM' => 'gateway',
+
+    'VHOST_G2_SERVERNAME' => 'g2',
+    'VHOST_G2_PROXY_URL' => 'http://upddd',
+    'VHOST_G2_BASIC_USERNAME' => 'hans',
+    'VHOST_G2_BASIC_PASSWORD' => 'hans',
+    'VHOST_G2_EXPOSE_PATH' => '/derDude',
+
+    'VHOST_G3_SERVERNAME' => 'g2',
+    'VHOST_G3_DEFAULT_SERVE' => '/var/www/hans',
+
+    'ENABLE_CONDITIONAL_BASIC_AUTH' =>"NO",
+
+];
+*/
+// END FOR DEV
+
+$outputFile = null;
+if (isset($argv[1])) {
+    $outputFile = $argv[1];
+}
+
+/**** PREPARE VHOSTS ARRAY
+ * we either accept 1 vhost configured (no VHOST_ prefix necessary)
+ * or multiple vhosts configured by VHOST_[NAME]_*.
+ * for our template we always prepare a nice array for the sub template
+ */
+
+$vhostPrefix = 'VHOST_';
+$vhosts = [];
+foreach ($_ENV as $envName => $envValue) {
+    if (substr($envName, 0, strlen($vhostPrefix)) == $vhostPrefix) {
+        $nameParts = explode('_', $envName);
+        if (count($nameParts) < 3) {
+            echo 'ERROR ON ENV "'.$envName.'" => not matching naming convention, skipping...'.PHP_EOL;
+            continue;
+        }
+
+        $vhosts[$nameParts[1]][implode('_', array_slice($nameParts, 2))] = $envValue;
+        unset($_ENV[$envName]);
+    }
+}
+
+// any vhosts set? if not -> fill with default
+if (empty($vhosts)) {
+    $_ENV['VHOSTS']['default'] = $_ENV;
+} else {
+    $_ENV['VHOSTS'] = $vhosts;
+}
+
+/***** DO STUFF PER VHOST *****/
+
+foreach ($_ENV['VHOSTS'] as $vhostName => $vhostSettings) {
+    // basic auth?
+    if (isset($vhostSettings['BASIC_USERNAME']) && isset($vhostSettings['BASIC_PASSWORD'])) {
+        // create basic auth file
+        $passwdFile = '/tmp/nginx_passwd_vhost_'.$vhostName;
+        shell_exec(
+            sprintf(
+                'htpasswd -bc %s %s %s',
+                escapeshellarg($passwdFile),
+                escapeshellarg($vhostSettings['BASIC_USERNAME']),
+                escapeshellarg($vhostSettings['BASIC_PASSWORD'])
+            )
+        );
+        $vhostSettings['BASIC_AUTH_FILE'] = $passwdFile;
+    }
+
+    // is conditional basic auth enabled somewhere? -> toggle for global map()
+    if (isset($vhostSettings['ENABLE_CONDITIONAL_BASIC_AUTH']) && $vhostSettings['ENABLE_CONDITIONAL_BASIC_AUTH'] == 'YES') {
+        $_ENV['CONDITIONAL_BASIC_AUTH_USED'] = true;
+    }
+
+    // proxy mode fpm?
+    if (isset($vhostSettings['FPM_UPSTREAM']) && isset($vhostSettings['FPM_PATH'])) {
+        $vhostSettings['PROXY_MODE'] = 'fpm';
+        $_ENV['GLOBAL_UPSTREAMS'][$vhostName] = $vhostSettings['FPM_UPSTREAM'];
+    }
+
+    if (isset($vhostSettings['PROXY_URL'])) {
+        $vhostSettings['PROXY_MODE'] = 'standard';
+        $_ENV['GLOBAL_UPSTREAMS'][$vhostName] = $vhostSettings['PROXY_URL'];
+    }
+
+    $envCopy = $_ENV;
+    if (isset($envCopy['VHOSTS'])) unset($envCopy['VHOSTS']);
+
+    $_ENV['VHOSTS'][$vhostName] = array_merge($envCopy, $vhostSettings);
+}
+
+/***** END DO STUFF PER VHOST ****/
+
+/***** RESOLVER PREPARATION *****/
+
+if ($_ENV['RESOLVER'] == 'none') {
+    // read out from resolv.conf
+    $_ENV['RESOLVER'] = getResolvers();
+}
+
+/******* END OF ALL *******/
+
+$environment = array_merge($_ENV, []);
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new Twig_Environment($loader, []);
+
+$mainTemplate = 'main.twig';
+if (is_null($outputFile)) {
+    echo $twig->render($mainTemplate, $environment);
+} else {
+    file_put_contents($outputFile, $twig->render($mainTemplate, $environment));
+    echo 'wrote configuration to '.$outputFile.PHP_EOL;
+}
+
+function getResolvers() {
+    $resolver = file_get_contents('/etc/resolv.conf');
+    preg_match_all('/nameserver (.*)/', $resolver, $matches);
+
+    if (empty($matches[1])) {
+        echo 'ERROR - COULD NOT DETERMINE RESOLVERS! PLEASE SET ENV "RESOLVER"!';
+        die;
+    }
+
+    return implode(' ', $matches[1]);
+}
