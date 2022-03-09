758d1b833768386609f79f7a4b72932698133468
Dario Nuevo <wee@xbe.ch>
2022-03-09 11:40:44 +0100

    support mtail

--- src/usr/local/bin/run.sh
+++ src/usr/local/bin/run.sh
@@ -1,5 +1,22 @@
 #!/bin/ash
 
+# mtail fifo?
+if [ ! -z "${MTAIL_PATH}" ]
+then
+    # always create if doesn't exist
+    if [ ! -p "${MTAIL_PATH}" ]
+    then
+        echo "--------- CREATING MTAIL FIFO AT ${MTAIL_PATH} ----------------"
+        mkfifo "${MTAIL_PATH}"
+    fi
+fi
+
+# start mtail?
+if [[ "true" == "${MTAIL_START}" ]]; then
+    exec /usr/bin/mtail -logtostderr -port 3093 -progs /etc/nginx/mtail/ -poll_interval 0 -logs "${MTAIL_PATH}"
+    exit 0
+fi
+
 /usr/bin/php -d variables_order=E /opt/configurator/configure.php /etc/nginx/nginx.conf
 
 #### generate environment.json file
